19a29a98a53d90db163af50de360a15ef82017f9
Dexsidius <dada1698@live.com>
2026-02-20 09:55:35 -0500

    Created Larry Site

---
+++ SiteLarry/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SitelarryConfig(AppConfig):
+    name = 'SiteLarry'
---
+++ SiteLarry/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+class PortfolioView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request):
+        return HttpResponse("Hello, World")
+

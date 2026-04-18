5e0bcb432da8787de5998147dcd1c1db3085c8ba
Dexsidius <dada1698@live.com>
2026-04-18 14:58:26 -0400

    pushed for setting up proper docker deployment

--- LarrySite/settings.py
+++ LarrySite/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.216',  '10.0.0.133']
 
 
 # Application definition

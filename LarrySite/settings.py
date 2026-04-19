663e3c4e8354f08d4d33d79b38e0758aba395f65
Dexsidius <dada1698@live.com>
2026-04-19 01:28:27 -0400

    fixing first template

--- LarrySite/settings.py
+++ LarrySite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.216',  '10.0.0.133']

7c92f9aeb48bd70a7886cf6d6024ec32a4650f76
Dexsidius <dada1698@live.com>
2026-04-19 01:16:48 -0400

    fixing first template

--- SiteLarry/views.py
+++ SiteLarry/views.py
@@ -6,6 +6,8 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class PortfolioView(TemplateView):
-    template_name = "index.html"
+    template_name = "templates/index.html"
 
+    def get(self, request):
+        return HttpResponse("Hellow, World")
 
